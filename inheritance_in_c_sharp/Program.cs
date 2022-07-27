29c9d7a3020da7594c8182f3fa198c99b100f018
SAM BHU <SAM BHU@DESKTOP-5MMOUET>
2022-07-27 10:39:42 +0545

    Add project files.

---
+++ inheritance_in_c_sharp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace inheritance_in_c_sharp
+{
+    //this is a base class
+    class shape {
+
+        public int lenght;
+        public int breath;
+
+        public void setLength(int l)
+        {
+            lenght = l;
+        }
+        public void setBreath(int b)
+        {
+            breath = b;
+        }
+
+    }
+    //this is a derived class
+
+    
+    class rectangle:shape{
+         
+         public int area()
+        {
+            return lenght * breath;
+        }
+        }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            rectangle rec = new rectangle();
+
+            rec.setLength(12);
+            rec.setBreath(12);
+            int area = rec.area();
+            Console.WriteLine("the total area is:{0}", area);
+
+        }
+    }
+}
